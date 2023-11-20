7ad4fddde2d5cb06247c03289dda4adfcc7e9399
Lucas Parise <69602364+lucasparise03@users.noreply.github.com>
2023-11-20 14:20:44 -0300

    Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -6,56 +6,43 @@ class Carro:
         self.transmissao = ""
         self.entretenimento = ""
 
-
 class CarroBuilder:
     def __init__(self):
         self.carro = Carro()
 
-
     def construir_modelo(self):
         pass
-
 
     def construir_cor(self):
         pass
 
-
     def construir_motor(self):
         pass
-
 
     def construir_transmissao(self):
         pass
 
-
     def construir_entretenimento(self):
         pass
 
-
     def obter_carro(self):
         return self.carro
-
 
 class CarroLuxoBuilder(CarroBuilder):
     def construir_modelo(self):
         self.carro.modelo = "Carro de Coleção"
 
-
     def construir_cor(self):
         self.carro.cor = "Vermelho Fosco"
-
 
     def construir_motor(self):
         self.carro.motor = "Motor de Foguete"
 
-
     def construir_transmissao(self):
         self.carro.transmissao = "Manual"
 
-
     def construir_entretenimento(self):
         self.carro.entretenimento = "Sistema de Som Intermediário"
-
 
 class Diretor:
     def construir_carro(self, builder):
@@ -65,7 +52,6 @@ class Diretor:
         builder.construir_transmissao()
         builder.construir_entretenimento()
 
-
 def exibir_carro(carro):
     print(f"Modelo: {carro.modelo}")
     print(f"Cor: {carro.cor}")
@@ -74,12 +60,9 @@ def exibir_carro(carro):
     print(f"Entretenimento: {carro.entretenimento}")
 
 
-
-
 builder_luxo = CarroLuxoBuilder()
 diretor = Diretor()
 diretor.construir_carro(builder_luxo)
 carro_luxo = builder_luxo.obter_carro()
-
 
 exibir_carro(carro_luxo)
